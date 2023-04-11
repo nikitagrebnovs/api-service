2773f1ff3ef0ddc6f37232a58710093d6e1ce061
Nikita Grebnovs <nikita.grebnovs@lendsecured.com>
2023-04-11 03:19:50 +0300

    remove unused routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,6 @@
 <?php
 
-use App\Http\Controllers\ExchangeController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::resource('products', App\Http\Controllers\ExchangeController::class);
 
 Route::get('/', function () {
     return view('welcome');
